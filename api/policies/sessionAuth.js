e148d5c180f557df6aab183bebaf67a441c0849b
Scott Vickers <scott.w.vickers@gmail.com>
2014-11-30 21:21:58 -0800

    allow authToken to appear in header, fix bug when clearing authToken from req params

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -8,17 +8,27 @@
  *
  */
 module.exports = function(req, res, next) {
-
+    'use strict';
+    /* global AuthToken */
     // TODO - turn this into a generic service
     function deleteAuthTokenParams() {
-        delete req.query.authToken
-        delete req.params.authToken
-        delete req.body.authToken
+        if (req.query && req.query.authToken) {
+            delete req.query.authToken;
+        }
+        if (req.params && req.params.authToken) {
+            delete req.params.authToken;
+        }
+        if (req.body && req.body.authToken) {
+            delete req.body.authToken;
+        }
     }
 
     // User is allowed, proceed to the next policy, 
     // or if this is the last policy, the controller
     var params = req.allParams();
+    if (!req.isSocket) {
+        params.authToken = params.authToken || req.get('authToken');
+    }
     if (!params.authToken) {
         // User is not allowed
         // (default res.forbidden() behavior can be overridden in `config/403.js`)
@@ -28,10 +38,13 @@ module.exports = function(req, res, next) {
     AuthToken.findOne({
         tokenData: params.authToken
     }).then(function(token) {
+        if (!token) {
+            return res.forbidden('You are not permitted to perform this action.');
+        }
         // Ensure the token hasn't expired
         var timeDiff = token.expiresAt - (new Date());
         if (timeDiff <= 0) {
-            return res.forbidden('Invalid token.');
+            return res.forbidden('Token expired.');
         }
 
         // remove the token info from the request
@@ -43,6 +56,7 @@ module.exports = function(req, res, next) {
 
         next();
     }).catch(function(e) {
-        return res.forbidden('Invalid token.');
+        req._sails.log.debug(e);
+        return res.forbidden('You are not permitted to perform this action.');
     });
 };
